6706512d19d767109fe29e7d07c9b0afc2c972c5
Mario Cao <mariocao@gmail.com>
2024-12-16 13:54:08 +0100

    chore(config): add devnet 4 seda core addresses

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,11 +11,47 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: '0.8.25',
   networks: {
+    arbitrumSepolia: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614,
+    },
     baseSepolia: {
       accounts: [process.env.EVM_PRIVATE_KEY || ""],
       url: 'https://sepolia.base.org',
       chainId: 84532,
-    }
+    },
+    berachainBartio: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://bartio.rpc.berachain.com',
+      chainId: 80084,
+    },
+    flowTestnet: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://testnet.evm.nodes.onflow.org',
+      chainId: 545,
+      minGasPrice: 100000000,
+    },
+    holesky: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://ethereum-holesky-rpc.publicnode.com',
+      chainId: 421614,
+    },
+    inkSepolia: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://rpc-gel-sepolia.inkonchain.com/',
+      chainId: 763373,
+    },
+    seiTestnet: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://evm-rpc-testnet.sei-apis.com',
+      chainId: 118,
+    },
+    unichainSepolia: {
+      accounts: [process.env.EVM_PRIVATE_KEY || ""],
+      url: 'https://sepolia.unichain.org',
+      chainId: 1301,
+    },
   },
   etherscan: {
     apiKey: process.env.BASE_SEPOLIA_ETHERSCAN_API_KEY || "",
@@ -26,6 +62,30 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: 'https://api-sepolia.basescan.org/api',
           browserURL: 'https://sepolia.basescan.org',
+        }
+      },
+      {
+        chainId: 1301,
+        network: 'unichainSepolia',
+        urls: {
+          apiURL: 'https://api-sepolia.uniscan.xyz/api',
+          browserURL: 'https://sepolia.uniscan.xyz',
+        }
+      },
+      {
+        chainId: 80084,
+        network: 'berachainBartio',
+        urls: {
+          apiURL: 'https://api.routescan.io/v2/network/testnet/evm/80084/etherscan/api',
+          browserURL: 'https://bartio.beratrail.io',
+        }
+      },
+      {
+        chainId: 421614,
+        network: 'arbitrumSepolia',
+        urls: {
+          apiURL: 'https://api-sepolia.arbiscan.io/api',
+          browserURL: 'https://sepolia.arbiscan.io',
         }
       }
     ]
--- seda.config.ts
+++ seda.config.ts
@@ -1,9 +1,30 @@
 export interface SedaConfig {
-  proverAddress: string;
+  coreAddress: string;
 }
 
 export const networkConfigs: { [network: string]: SedaConfig } = {
+  arbitrumSepolia: {
+    coreAddress: "0xb5b83ca54daeb0c5eec63dac7c92981996083b4a",
+  },
   baseSepolia: {
-    proverAddress: "0xcBC8a3159535BfE276ADaA8604940602e02c5457",
-  }
+    coreAddress: "0x42ee3e29ebae162d43a9efd93224a9bd32d1ebc4",
+  },
+  berachainBartio: {
+    coreAddress: "0x511eac94EF52b2bb358a5162db632F38987591bF",
+  },
+  flowTestnet: {
+    coreAddress: "0xaa88065032A8ee4C364Cb3C69744A9f976402388",
+  },
+  holesky: {
+    coreAddress: "0x83D2660c4aE8F5685B022c3e6bc9F6D3Ef60eF45",
+  },
+  inkSepolia: {
+    coreAddress: "0x93EAE6368b56383485645d2EebdCE4fF9f77d72C",
+  },
+  seiTestnet: {
+    coreAddress: "0x83D2660c4aE8F5685B022c3e6bc9F6D3Ef60eF45",
+  },
+  unichainSepolia: {
+    coreAddress: "0xFFCd728685029dA7c72875C1c03b6960E3d94334",
+  },
 };
